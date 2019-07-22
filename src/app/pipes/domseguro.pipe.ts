4aa67397bf16442ef289bf506d95e22c2ef146a0
paulQR <paulqr10@gmail.com>
2019-07-22 15:32:26 +0200

    2 files modified

--- src/app/pipes/domseguro.pipe.ts
+++ src/app/pipes/domseguro.pipe.ts
@@ -9,8 +9,13 @@ export class DomseguroPipe implements PipeTransform {
 
   constructor( private domSanitizer:DomSanitizer ){ }
 
+  /*
   transform( value: string, url: string): any {
     return this.domSanitizer.bypassSecurityTrustResourceUrl( url + value );
   }
-
+  */
+  transform( value: string): any {
+    const url = 'htttps://open.spotify.com/embed?uri=';
+    return this.domSanitizer.bypassSecurityTrustResourceUrl( url + value );
+  }
 }
